a933118128fa28717b46b7237d7205a64285e978
Kushank <singh.kushank12@gmail.com>
2022-01-22 18:04:12 -0500

    Updated

--- app.js
+++ app.js
@@ -2,16 +2,49 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var Finance = require('financejs');
+var finance = new Finance();
+
+
+
+
 
 const app = express();
+
+// parse application/json
+app.use(bodyParser.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/apr", function(req, res){
+app.get("/apr.html", function(req, res){
     res.sendFile(__dirname + "/apr.html");
 });
+
+app.post("/apr.html", function(req, res) {
+    // console.log('Auth request recieved');
+    const principal = Number(req.body.principal);
+    const rate = Number(req.body.rate);
+    const tp = Number(req.body.tp);
+    const type = Number(req.body.type);
+    // console.log("principal: " + req.body.principal);
+    // console.log("tp: " + req.body.tp)
+    // console.log("type: " + req.body.type)
+    // console.log("rate: "+req.body.rate)
+    // finance.AM(principal, rate, total number of payments, [type]);
+    let APR = (finance.AM(principal, rate, tp, type));
+    console.log("APR:" + APR)
+    res.send("APR " + APR);
+});
+
+
+
+
+
+
 
 
 app.listen(3000, function(){
